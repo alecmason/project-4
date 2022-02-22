9f978501bb9103a93183f70ca557416907e32a0a
Alec Jones <alecjones@Alecs-MacBook-Air.local>
2022-02-22 13:42:24 -0600

    create signup and login pages

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,20 +1,98 @@
 import React, { useState } from "react";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
-
+import { Button, Form, Grid, Header, Image, Segment } from "semantic-ui-react";
 import userService from "../../utils/userService";
+
 import { useNavigate } from "react-router-dom";
 
 export default function SignUpPage(props) {
+  const [error, setError] = useState("");
+  const [state, setState] = useState({
+    username: "",
+    email: "",
+    password: "",
+    passwordConf: "",
+    bio: "",
+  });
+
+  const navigate = useNavigate(); // navigate hook from react-router
+
+  function handleChange(e) {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      await userService.signup(state);
+      // after we signup, we can navigare/and decode our token and set in local storage
+      props.handleSignUpOrLogin(); // <- get the token from localstorage and decode it
+      // and set the user state in the App.js componennt
+      navigate("/"); // < route the user to our home component (all our routes are defined in App.js)
+    } catch (err) {
+      // err, is defined in the throw new Error in the
+      // userServiceSignUp
+      setError(err.message);
+    }
+  }
+
   return (
-    <>
-      <h1>Signup PAGE</h1>
-      <ul>
-        <li>Read the Login Model, You can change it to fit your needs</li>
-        <li>
-          Make sure you read the Login Controller, to know how it is setup to
-          find the user!
-        </li>
-      </ul>
-    </>
+    <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h2" color="blue" textAlign="center">
+          Sign Up
+        </Header>
+        <Form autoComplete="off" onSubmit={handleSubmit}>
+          <Segment stacked>
+            <Form.Input
+              name="username"
+              placeholder="username"
+              value={state.username}
+              onChange={handleChange}
+              required
+            />
+            <Form.Input
+              type="email"
+              name="email"
+              placeholder="email"
+              value={state.email}
+              onChange={handleChange}
+              required
+            />
+            <Form.Input
+              name="password"
+              type="password"
+              placeholder="password"
+              value={state.password}
+              onChange={handleChange}
+              required
+            />
+            <Form.Input
+              name="passwordConf"
+              type="password"
+              placeholder="Confirm Password"
+              value={state.passwordConf}
+              onChange={handleChange}
+              required
+            />
+            <Form.TextArea
+              label="bio"
+              name="bio"
+              placeholder="Tell us about you (Your current role i.e. Frontend Developer, Student) and your current interests."
+              onChange={handleChange}
+            />
+
+            <Button type="submit" className="btn">
+              Signup
+            </Button>
+          </Segment>
+          {error ? <ErrorMessage error={error} /> : null}
+        </Form>
+      </Grid.Column>
+    </Grid>
   );
 }
